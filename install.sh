4771d326df66712393ac50d708a7a335acf7768a
JeongwonKim <30548814+swtpumpkin@users.noreply.github.com>
2020-03-30 00:10:15 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,5 @@
 #! /bin/bash
 
-echo "Start Brew Install ..."
-&& bash brew.sh
+bash brew.sh && bash tree.sh && bash nvm.sh && bash zsh.sh && bash ohMyZsh.sh && bash zshSyntaxHighlighting.sh && source ~/.zshrc && bash backtick.sh && bash bulletTrain.sh && bash node.sh && bash gitSet.sh
 
-&& echo "Start Brew Tree Install ..."
-&& bash tree.sh
-
-&& echo "Start NVM Install ..."
-&& bash nvm.sh
-
-&& echo "Start ZSH Install ..."
-&& bash zsh.sh
-
-&& echo "Start oh-my-zsh Install ..."
-&& bash ohMyZsh.sh
-
-&& echo "Start zsh-syntax-highlighting Install ..."
-&& bash zshSyntaxHighlighting.sh
-
-&& echo "Source zshrc ..."
-&& source ~/.zshrc
-
-&& echo "Start backtick Set ..."
-&& bash backtick.sh
-
-&& echo "Start bullet train Set ..."
-&& bash bulletTrain.sh
-
-&& echo "Start Node Set ..."
-&& bash node.sh
-
-&& echo "Start Git Set ..."
-&& bash gitSet.sh
-
-&& echo "Done"
+echo "Done"

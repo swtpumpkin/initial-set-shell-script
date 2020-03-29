d2c37c9952c7189583a86ae7a14f12fad8d98b5e
JeongwonKim <30548814+swtpumpkin@users.noreply.github.com>
2020-03-30 00:06:59 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,36 @@
 #! /bin/bash
 
-echo "Start Brew Install ..." \
-&& bash brew.sh \
+echo "Start Brew Install ..."
+&& bash brew.sh
 
-&& echo "Start Brew Tree Install ..." \
-&& bash tree.sh \
+&& echo "Start Brew Tree Install ..."
+&& bash tree.sh
 
-&& echo "Start NVM Install ..." \
-&& bash nvm.sh \
+&& echo "Start NVM Install ..."
+&& bash nvm.sh
 
-&& echo "Start ZSH Install ..." \
-&& bash zsh.sh \
+&& echo "Start ZSH Install ..."
+&& bash zsh.sh
 
-&& echo "Start oh-my-zsh Install ..." \
-&& bash ohMyZsh.sh \
+&& echo "Start oh-my-zsh Install ..."
+&& bash ohMyZsh.sh
 
-&& echo "Start zsh-syntax-highlighting Install ..." \
-&& bash zshSyntaxHighlighting.sh \
+&& echo "Start zsh-syntax-highlighting Install ..."
+&& bash zshSyntaxHighlighting.sh
 
-&& echo "Source zshrc ..." \
-&& source ~/.zshrc \
+&& echo "Source zshrc ..."
+&& source ~/.zshrc
 
-&& echo "Start backtick Set ..." \
-&& bash backtick.sh \
+&& echo "Start backtick Set ..."
+&& bash backtick.sh
 
-&& echo "Start bullet train Set ..." \
-&& bash bulletTrain.sh \
+&& echo "Start bullet train Set ..."
+&& bash bulletTrain.sh
 
-&& echo "Start Node Set ..." \
-&& bash node.sh \
+&& echo "Start Node Set ..."
+&& bash node.sh
 
-&& echo "Start Git Set ..." \
-&& bash gitSet.sh \
+&& echo "Start Git Set ..."
+&& bash gitSet.sh
 
 && echo "Done"

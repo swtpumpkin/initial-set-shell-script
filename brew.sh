f3a653826d4e912bdbbf2452ce0feea7bdd6573d
JeongwonKim <30548814+swtpumpkin@users.noreply.github.com>
2020-03-29 23:18:20 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
+install_homebrew() {
+  echo "Installing brew ..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+}
+
 if [[ -s /usr/local/bin/brew ]] ; then
   echo "Already Installed ..."
 else
-  echo "Installing brew ..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  install_homebrew
 fi

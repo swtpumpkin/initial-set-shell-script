11acc1c338d757d108db432114893d38808e1421
JeongwonKim <30548814+swtpumpkin@users.noreply.github.com>
2020-03-29 20:04:47 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -7,8 +7,10 @@ echo "Installing nvm ..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 echo "Adding nvm path ..."
-echo "export NVM_DIR="$HOME/.nvm"" >> ~/.bash_profile
-echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\" # This loads nvm" >> ~/.bash_profile
+sudo cat > ~/.bash_profile << EOF
+export NVM_DIR=/Users/swtpumpkin/.nvm
+[ -s "/nvm.sh" ] && . "/nvm.sh" # This loads nvm
+EOF
 
 echo "Source bash_profile ..."
 source ~/.bash_profile

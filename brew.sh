adfa93c066788e566edf77e236240746c1004b91
JeongwonKim <30548814+swtpumpkin@users.noreply.github.com>
2020-03-30 00:04:09 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -4,5 +4,5 @@ if [[ -s /usr/local/bin/brew ]] ; then
   echo "Already Installed ..."
 else
   echo "Installing brew ..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(wget -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi

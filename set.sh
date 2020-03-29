cf4b581e8a3498f5b7bc3bf5bfc3e20a7dafda04
JeongwonKim <30548814+swtpumpkin@users.noreply.github.com>
2020-03-29 09:31:51 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-echo "Installing brew ..."
-sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 echo "Installing nvm ..."
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 

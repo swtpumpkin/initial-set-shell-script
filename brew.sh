f9b7722f0c46deecb61f328e8900bc09e9741554
JeongwonKim <30548814+swtpumpkin@users.noreply.github.com>
2020-03-29 23:07:38 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 echo "Installing brew ..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
